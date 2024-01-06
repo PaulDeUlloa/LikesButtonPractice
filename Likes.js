899275b5450b594daa7f16a3ab30109cfe9adb8d
Paul De Ulloa <pauld143@gmail.com>
2024-01-05 23:53:02 -0800

    LikesButtonPractice

---
+++ Likes.js
@@ -0,0 +1,25 @@
+var count = 9;
+var countElement = document.querySelector("#count");
+
+function add1(){
+    count++;
+    countElement.innerText = count + " like(s)"
+}
+
+var countMiddle = 12;
+var countMiddleElement = document.querySelector("#countMiddle");
+
+function addMiddle(){
+    countMiddle++;
+    countMiddleElement.innerText = countMiddle + " like(s)"
+
+}
+
+var countThird = 9;
+var countThirdElement = document.querySelector("#countThird");
+
+function addThird(){
+    countThird++;
+    countThirdElement.innerText = countThird + " like(s)"
+
+}
